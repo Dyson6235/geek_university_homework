3e975c837c9d1ff174097a41077fe2b40ea8eeba
Dyson6235 <ghostpants1111@163.com>
2020-07-27 10:26:46 +0800

    firstHomework

---
+++ SolutionForMoveZero.java
@@ -0,0 +1,15 @@
+public class SolutionForMoveZero {
+    public void moveZero(int[] nums){
+        int j=0;
+        for(int i=0;i<nums.length;++i){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                if(j!=i){
+                    nums[i]=0;
+                }
+                j++;
+            }
+            
+        }
+    }
+}
